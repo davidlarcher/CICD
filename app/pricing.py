4dae3c567cf599af4d40e3968f802a6f0c010f14
David <david.larcher1@gmail.com>
2026-09-18 18:02:33 +0200

    removed vulnerability

--- app/pricing.py
+++ app/pricing.py
@@ -7,6 +7,3 @@ def calculate_price(
         raise ValueError("Quantity must be positive")
 
     return unit_price * quantity
-
-def calculate_discount(expression: str):
-    return eval(expression)
